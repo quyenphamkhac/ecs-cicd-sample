155d8edbc65fa358ba4f2d7854f1d5726a952521
Quyen Pham Khac <phamkhacquyen1996@gmail.com>
2022-04-12 14:42:31 +0700

    feat: test health controller

---
+++ controller/health_ctrl_test.go
@@ -0,0 +1,42 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHealthCheckResponse(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	t.Run("200OK", func(t *testing.T) {
+		r := gin.New()
+		r.GET("/health", NewHealthController().Health)
+		rr := httptest.NewRecorder()
+		req, err := http.NewRequest(http.MethodGet, "/health", nil)
+		assert.NoError(t, err)
+		r.ServeHTTP(rr, req)
+		assert.Equal(t, 200, rr.Code)
+	})
+
+	t.Run("SuccessResponse", func(t *testing.T) {
+		r := gin.New()
+		r.GET("/health", NewHealthController().Health)
+		rr := httptest.NewRecorder()
+		req, err := http.NewRequest(http.MethodGet, "/health", nil)
+		assert.NoError(t, err)
+		r.ServeHTTP(rr, req)
+
+		expected, err := json.Marshal(gin.H{
+			"message": "OK",
+		})
+
+		assert.NoError(t, err)
+		assert.Equal(t, 200, rr.Code)
+		assert.Equal(t, expected, rr.Body.Bytes())
+	})
+}
