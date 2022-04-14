bb937865471a7b454085e933e1d990c53c84c809
Quyen Pham Khac <phamkhacquyen1996@gmail.com>
2022-04-14 14:32:23 +0700

    feat: trigger code pipeline

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/quyenphamkhac/ecs-cicd-sample/server"
 )
 
 func main() {
+	fmt.Println("Hello World")
 	s := server.NewHttpServer()
 	s.Run(":8080")
 }
