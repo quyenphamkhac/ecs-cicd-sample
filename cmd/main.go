e45f5acb40ebd16bbddc174fc240e072f760c126
Quyen Pham Khac <phamkhacquyen1996@gmail.com>
2022-04-19 14:48:42 +0700

    feat:xxx

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/quyenphamkhac/ecs-cicd-sample/server"
 )
 
 func main() {
-	fmt.Println("Hello World")
 	s := server.NewHttpServer()
 	s.Run(":8080")
 }
